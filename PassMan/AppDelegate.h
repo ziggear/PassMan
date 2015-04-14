04cfcedb356b68a4a170cedb3ddb16b9ed15de8e
Ziggear <ziggear@gmail.com>
2015-04-14 12:21:13 +0800

    Initial Commit

---
+++ PassMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PassMan
+//
+//  Created by ziggear on 15-4-14.
+//  Copyright (c) 2015年 ziggear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PassMan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PassMan
+//
+//  Created by ziggear on 15-4-14.
+//  Copyright (c) 2015年 ziggear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
